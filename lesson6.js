990a71f06a6df6524391ef08cff818510a0fa8c8
fleish80 <Fleish.D@gmail.com>
2018-12-16 22:13:30 +0200

    Lesson 6

---
+++ lesson6.js
@@ -0,0 +1,11 @@
+const items = [1, 5, 2, 7, 3, 12, 6, 10]
+items.sort((a, b) => a - b);
+
+console.log(items);
+
+{
+    function search(list, item) {
+        const index = Math.floor(list.length / 2);
+        
+    }
+}
