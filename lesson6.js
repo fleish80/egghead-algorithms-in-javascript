50db1eee063cafab8bf7f60cc17a1e81d0701b19
fleish80 <Fleish.D@gmail.com>
2018-12-18 22:12:11 +0200

    Lesson 6

--- lesson6.js
+++ lesson6.js
@@ -4,8 +4,54 @@ items.sort((a, b) => a - b);
 console.log(items);
 
 {
+    const items = [1, 5, 2, 7, 3, 12, 6, 10];
+
+    // O(n)
+    // Once for each element in the array
+
     function search(list, item) {
-        const index = Math.floor(list.length / 2);
-        
+        let hasItem = null;
+        let counter = 0;
+
+        for (let e of list) {
+            counter++;
+            if (e === item) {
+                hasItem = true;
+                break
+            }
+        }
+        console.log('counter0', counter);
+        return hasItem;
     }
+
+    console.log(search(items, 12));
+}
+
+{
+    const items = [1, 5, 2, 7, 3, 12, 6, 10];
+
+    // O(n)
+    // Once for each element in the array
+
+    items.sort((a, b) => a - b);
+    console.log(items);
+
+    function search(list, item) {
+        let low = 0
+        let high = list.length
+        let counter = 0
+      
+      while(low <= high) {
+        counter++
+        let mid = Math.floor((low + high) / 2)
+        let guess = list[mid]
+        if(guess === item) return true
+        if(guess > item) high = mid - 1
+        else low = mid + 1 
+      }
+        console.log('counter1', counter);
+        return null;
+      }
+
+    console.log('search1', search(items, 12));
 }
