668ddd9ca7dac4a6fc865ff64d7e830d78d3e668
fleish80 <Fleish.D@gmail.com>
2018-12-19 21:20:17 +0200

    Lesson 8

--- lesson7.js
+++ lesson7.js
@@ -3,32 +3,32 @@
 const items = [9,6,5, 1];
 const size = 4;
 
-    minCoins(x) 
+    function minCoins(x) 
     { 
           
-        // base case 
-        if (x == 0) return 0; 
+        // // base case 
+        // if (x == 0) return 0; 
           
-        // Initialize result 
-        // int res = int.MaxValue; 
+        // // Initialize result 
+        // // int res = int.MaxValue; 
           
-        // Try every coin that has 
-        // smaller value than V 
-        for(let item of items) 
-        { 
-            if (item <= x) 
-            { 
-                int sub_res = minCoins(coins, m, 
-                                  V - coins[i]); 
+        // // Try every coin that has 
+        // // smaller value than V 
+        // for(let item of items) 
+        // { 
+        //     if (item <= x) 
+        //     { 
+        //         int sub_res = minCoins(coins, m, 
+        //                           V - coins[i]); 
           
-                // Check for INT_MAX to  
-                // avoid overflow and see  
-                // if result can minimized 
-                if (sub_res != int.MaxValue &&  
-                            sub_res + 1 < res) 
-                    res = sub_res + 1; 
-            } 
-        } 
-        return res; 
+        //         // Check for INT_MAX to  
+        //         // avoid overflow and see  
+        //         // if result can minimized 
+        //         if (sub_res != int.MaxValue &&  
+        //                     sub_res + 1 < res) 
+        //             res = sub_res + 1; 
+        //     } 
+        // } 
+        // return res; 
     } 
 }
